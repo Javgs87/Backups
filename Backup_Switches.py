fd63103aaf7d3352f4a09eaf43837ab0b9df643d
Javgs87 <52715997+Javgs87@users.noreply.github.com>
2019-07-09 16:38:40 -0500

    Update Backup_Switches.py

--- Backup_Switches.py
+++ Backup_Switches.py
@@ -1,1 +1,34 @@
-Respaldo Equipos
+#!/usr/bin/env python
+
+import getpass
+import sys
+import telnetlib
+
+#Username y Password
+user = raw_input("Ingresa tu usuario: ")
+password = getpass.getpass()
+
+#Abre el archivo donde estan todas las ips de los switches
+f = open("lista_switches")
+
+#Telnet a los equipos
+for line in f:
+        print "Obteniendo configuraciòn " + (line)
+        HOST = line.strip()
+        tn = telnetlib.Telnet(HOST)
+
+        tn.read_until("Username: ")
+        tn.write(user + "\n")
+
+        if password:
+                tn.read_unitl("Password: ")
+                tn.write(password + "\n")
+
+        tn.write("terminal length 0\n")
+        tn.write("show run\n")
+        tn.write("exit\n")
+
+readoutput = tn.read_all()
+saveoutput = open("switch" + HOST, "w")
+saveoutput.write(readoutput)
+saveoutput.close
